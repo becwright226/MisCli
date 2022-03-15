405ea64fc04f4a91d66722ed4b84dabe40ce3fca
becwright226 <becwright226@gmail.com>
2022-03-15 15:57:13 -0400

    git commit -m 'fixed it?'

--- typescript-day-01/Challenges/challenge2.ts
+++
@@ -1,8 +0,0 @@
-// PROBLEM 2
-
-function convertToDegreeCelcus(temps: number[]) {
-  return temps.map((singleTemp) => Math.floor(((+singleTemp - 32) * 5) / 9));
-}
-
-const dailyTemps = ["93", "78", "88"];
-console.log(convertToDegreeCelcus(dailyTemps));
